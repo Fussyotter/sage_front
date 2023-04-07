b74f8c2dc8d25627dc159ebdc5d5e0207b0dec1c
ialoysious <isaacaloysious@gmail.com>
2023-04-07 12:57:08 -0700

    working chat between front/back

--- sage/pages/api/hello.js
+++ sage/pages/api/hello.js
@@ -3,3 +3,21 @@
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
+export async function fetchChat() {
+	const response = await fetch('http://localhost:8000/chat/chattwo');
+	const data = await response.json();
+	return data;
+}
+
+export async function chatTest() {
+	const response = await fetch('http://localhost:8000/chat/chattwo/', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ content: 'bigtest' }),
+	});
+	console.log(response);
+	const data = await response.json();
+	return data;
+}
---
+++ sage/pages/chatDisplay.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { fetchChat, chatTest } from './api/hello';
+
+export default function ChatDisplay() {
+	const [orders, setOrders] = useState([]);
+
+	useEffect(() => {
+		async function fetchChatData() {
+			const data = await fetchChat();
+			setOrders(data);
+		}
+
+		fetchChatData();
+	}, []);
+
+	return (
+		<>
+			<div>
+				<button onClick={chatTest}>yea pls god</button>
+			</div>
+			<table>
+				<thead>
+					<tr>
+						<th>Order ID</th>
+						<th>Customer Name</th>
+						<th>Product Name</th>
+					</tr>
+				</thead>
+				<tbody>
+					{orders.map((order) => (
+						<tr key={order.id}>
+							<td>{order.id}</td>
+							<td>{order.content}</td>
+							<td>{order.recipient}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</>
+	);
+}
