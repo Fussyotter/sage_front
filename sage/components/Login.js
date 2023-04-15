d01bc26df121292599d71f14ae7575d7b4ddead6
ialoysious <isaacaloysious@gmail.com>
2023-04-15 14:16:06 -0700

    testing

--- sage/components/Login.js
+++ sage/components/Login.js
@@ -1,76 +1,77 @@
-import React, { useContext, useState } from "react";
-import { login, logout } from "@/functions/auth";
-import { UserContext } from "@/context/context";
+import React, { useContext, useState } from 'react';
+import { login, logout } from '@/functions/auth';
+import { UserContext } from '@/context/context';
 
 export default function Login() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    // not entirely sure i'll need these next two
-    const [loginStatus, setLoginStatus] = useState(null);
-    const [user, setUser] = useState(null);
+	const [loginStatus, setLoginStatus] = useState(false);
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [loggedInUser, setLoggedInUser] = useContext(UserContext);
 
-    // trying context out
-    const [loggedInUser, setLoggedInUser] = useContext(UserContext)
-    
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        try {
-            const {user} = await login({
-                username: username,
-                password: password,
-            });
-            setLoggedInUser(user.username);
+	const handleLogin = async (e) => {
+		e.preventDefault();
+		try {
+			const { user } = await login({
+				username: username,
+				password: password,
+			});
+			setLoggedInUser(user.username);
+			setLoginStatus(true);
+		} catch (error) {
+			console.error('login failed:', error);
+			setLoginStatus(false);
+		}
+	};
 
-            setUser(user);
-            setLoginStatus(true);
+	const handleLogout = async () => {
+		try {
+			await logout();
+			console.log('logout successful');
+			setLoggedInUser(null);
+			setLoginStatus(false);
+		} catch (error) {
+			console.error('logout failed', error);
+			setLoginStatus(false);
+		}
+	};
 
-        } catch(error) {
-            console.error('login failed:',error);
-            setLoginStatus(false);
-            setUser(null);
-        }
-    }
-    const handleLogout = async () => {
-        try {
-            await logout();
-            console.log('logout successful');
-            setLoggedInUser(null);
+	const LoginButton = () => {
+		return <button className='login-button'onClick={() => setLoginStatus(true)}>Log in</button>;
+	};
 
-            setLoginStatus(false);
-            setUser(null);
-        } catch (error) {
-            console.error('logout failed', error);
-            setLoginStatus(false);
-            setUser(null);
-        }
-    }
-
-    return (
-			<>
-				<div className='login-container'>
-					{!loginStatus ? (
-						<form className='login-form' onSubmit={handleLogin}>
-							<label htmlFor='username'>Username:</label>
-							<input
-								type='text'
-								id='username'
-								value={username}
-								onChange={(e) => setUsername(e.target.value)}
-							/>
-							<label htmlFor='password'>Password:</label>
-							<input
-								type='password'
-								id='password'
-								value={password}
-								onChange={(e) => setPassword(e.target.value)}
-							/>
-							<button type='submit'>Log in</button>
-						</form>
-					) : (
-						<button onClick={handleLogout}>Log out</button>
-					)}
-				</div>
-				
-			</>
+	const LoginForm = () => {
+		return (
+			<form className='login-form' onSubmit={handleLogin}>
+				<label htmlFor='username'>Username:</label>
+				<input
+					type='text'
+					id='username'
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<label htmlFor='password'>Password:</label>
+				<input
+					type='password'
+					id='password'
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<button type='submit'>Log in</button>
+			</form>
 		);
+	};
+
+	return (
+		<div className='login-container'>
+			{loginStatus ? <LoginForm /> : <LoginButton />}
+			{loginStatus && <button onClick={handleLogout}>Log out</button>}
+
+			<style>{`
+                input {
+                    display: block;
+                    margin-bottom: 10px;
+                }
+            `}</style>
+		</div>
+	);
 }
