bee10acbf643593e7ba75ef524db52b1890ab109
ialoysious <isaacaloysious@gmail.com>
2023-05-26 12:35:55 -0700

    changes

--- sage/components/GiftParams.js
+++ sage/components/GiftParams.js
@@ -26,27 +26,28 @@ export default function GiftParams() {
 		// console.log(gift);
 	}, [gift]);
 
-	const handleClick = async () => {
-		setMoving((prevMoving) => !prevMoving);
-		const response = await fetch('/api/postReq', {
-			method: 'POST',
-			body: JSON.stringify({ relationship, interest, }),
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
-		const data = await response.text();
-		const trimmedResponse = data.trim();
-		const formattedResponse = trimmedResponse.replace(/\n\n(\d+\.\s)/g, '\n\n$1');
+	const callApi = async (relationship, interest) => {
+  const response = await fetch('/api/postReq', {
+    method: 'POST',
+    body: JSON.stringify({ relationship, interest }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await response.text();
+  const trimmedResponse = data.trim();
+  const formattedResponse = trimmedResponse.replace(/\n\n(\d+\.\s)/g, '\n\n$1');
+  console.log(formattedResponse)
+  const json = JSON.parse(formattedResponse);
+  return json;
+};
 
-		const json = JSON.parse(formattedResponse);
-		console.log(json);
-		setGift(json.choices[0].text);
-		setMoving((prevMoving) => !prevMoving);
-
-
-
-	};
+const handleClick = async () => {
+  setMoving((prevMoving) => !prevMoving);
+  const json = await callApi(relationship, interest);
+  setGift(json.choices[0].text);
+  setMoving((prevMoving) => !prevMoving);
+};
 
 	return (
 		<div className='gift-params-container'>
@@ -67,70 +68,13 @@ export default function GiftParams() {
 			<button className='submit-button' onClick={handleClick}>
 				Get Gift Idea
 			</button>
-			{/* {gift && (
-				<div className="gift-result-wrapper">
-					<p>Your gift idea:</p>
-					<div className='gift-result'dangerouslySetInnerHTML={{ __html: gift }}></div>
-				</div> */}
-			{/* )} */}
 		</div>
 	);
 }
-// import React, { useState } from 'react';
-{/* {gift && gift.choices && gift.choices[0] && (
-	<div className='gift-result-container'>
-		<p className='gift-result-text'>{gift.choices[0].text}</p>
+
+{/* {gift && (
+	<div className="gift-result-wrapper">
+		<p>Your gift idea:</p>
+		<div className='gift-result'dangerouslySetInnerHTML={{ __html: gift }}></div>
 	</div> */}
 {/* )} */}
-
-// export default function GiftParams() {
-// 	const [gift, setGift] = useState('');
-// 	const [recipient, setRecipient] = useState('');
-// 	const [relationship, setRelationship] = useState('');
-// 	const [interest, setInterest] = useState('');
-// 	const [interest2, setInterest2] = useState('');
-
-// 	const handleClick = async () => {
-// 		const response = await fetch('/api/postReq', {
-// 			method: 'POST',
-// 			body: JSON.stringify({ recipient, relationship, interest, interest2 }),
-// 			headers: {
-// 				'Content-Type': 'application/json',
-// 			},
-// 		});
-// 		const data = await response.json();
-// 		setGift(data);
-//         console.log(gift)
-// 	};
-
-// 	return (
-// 		<>
-// 			<input
-// 				value={recipient}
-// 				onChange={(e) => setRecipient(e.target.value)}
-// 				placeholder='Recipient'
-// 			/>
-// 			<input
-// 				value={relationship}
-// 				onChange={(e) => setRelationship(e.target.value)}
-// 				placeholder='Relationship'
-// 			/>
-// 			<input
-// 				value={interest}
-// 				onChange={(e) => setInterest(e.target.value)}
-// 				placeholder='Interest 1'
-// 			/>
-// 			<input
-// 				value={interest2}
-// 				onChange={(e) => setInterest2(e.target.value)}
-// 				placeholder='Interest 2'
-// 			/>
-// 			<button onClick={handleClick}>Get Gift Idea</button>
-			
-//             <div>
-//                       {gift && <p>{gift.choices[0].text}</p>}
-
-//             </div>
-// 		</>
-// 	);
-// }
--- sage/pages/api/postReq.js
+++ sage/pages/api/postReq.js
@@ -1,43 +1,37 @@
-import cors from 'cors';
+import Cors from 'cors';
 
-const corsMiddleware = cors({
-	methods: ['GET', 'POST', 'PUT', 'DELETE'],
-	origin: '*',
-});
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(400).json({ message: 'Only POST requests allowed' });
+  }
+  const {
+    recipient = '',
+    relationship = '',
+    interest = '',
+  } = req.body;
 
-export default function handler(req, res) {
-	if (req.method !== 'POST') {
-		return res.status(400).json({ message: 'Only POST requests allowed' });
-	}
-	const {
-		recipient = '',
-		relationship = '',
-		interest = '',
-	} = req.body;
+  try {
+    const apiKey = process.env.OPENAI_API_KEY;
+    await corsMiddleware(req, res); // Await the corsMiddleware function
+    const prompt = `Give me a gift idea for  my "${relationship}" they like "${interest}".  5 items in an html list that are also links. Can I also have the response in magical tone with line breaks after each sentence.  Also tell my fortune`;
 
-	const prompt = `Give me a gift idea for  my "${relationship}" they like "${interest}".  5 items in an html list that are also links. Can I also have the response in magical tone with line breaks after each sentence.  Also tell my fortune`;
-
-	corsMiddleware(req, res, async () => {
-		try {
-			const apiKey = process.env.OPENAI_API_KEY;
-			const response = await fetch('https://api.openai.com/v1/completions', {
-				method: 'POST',
-				headers: {
-					Authorization: `Bearer ${apiKey}`,
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					prompt,
-					model: 'text-davinci-003',
-					max_tokens: 1500,
-					temperature: 1.0,
-				}),
-			});
-			const data = await response.json();
-			res.status(200).json(data);
-		} catch (error) {
-			console.error(error);
-			res.status(500).json({ message: 'Internal server error' });
-		}
-	});
+    const response = await fetch('https://api.openai.com/v1/completions', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        prompt,
+        model: 'text-davinci-003',
+        max_tokens: 1500,
+        temperature: 1.0,
+      }),
+    });
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 }
