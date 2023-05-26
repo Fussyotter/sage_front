bdd0721e10bd6857d9cef9d3ad0f2af50e6350f6
ialoysious <isaacaloysious@gmail.com>
2023-05-26 12:50:11 -0700

    updated a little

--- sage/pages/api/postReq.js
+++ sage/pages/api/postReq.js
@@ -1,37 +1,39 @@
-import Cors from 'cors';
+import cors from 'cors';
 
-export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(400).json({ message: 'Only POST requests allowed' });
-  }
-  const {
-    recipient = '',
-    relationship = '',
-    interest = '',
-  } = req.body;
+const corsMiddleware = cors({
+	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+	origin: '*',
+});
 
-  try {
-    const apiKey = process.env.OPENAI_API_KEY;
-    await corsMiddleware(req, res); // Await the corsMiddleware function
-    const prompt = `Give me a gift idea for  my "${relationship}" they like "${interest}".  5 items in an html list that are also links. Can I also have the response in magical tone with line breaks after each sentence.  Also tell my fortune`;
+export default function handler(req, res) {
+	if (req.method !== 'POST') {
+		return res.status(400).json({ message: 'Only POST requests allowed' });
+	}
+	const { recipient = '', relationship = '', interest = '' } = req.body;
 
-    const response = await fetch('https://api.openai.com/v1/completions', {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        prompt,
-        model: 'text-davinci-003',
-        max_tokens: 1500,
-        temperature: 1.0,
-      }),
-    });
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
+	const prompt = `Give me a gift idea for  my "${relationship}" they like "${interest}".  5 items in an html list that are also links. Can I also have the response in magical tone with line breaks after each sentence.  Also tell my fortune`;
+
+	corsMiddleware(req, res, async () => {
+		try {
+			const apiKey = process.env.OPENAI_API_KEY;
+			const response = await fetch('https://api.openai.com/v1/completions', {
+				method: 'POST',
+				headers: {
+					Authorization: `Bearer ${apiKey}`,
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					prompt,
+					model: 'text-davinci-003',
+					max_tokens: 800,
+					temperature: 1.0,
+				}),
+			});
+			const data = await response.json();
+			res.status(200).json(data);
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({ message: 'Internal server error' });
+		}
+	});
 }
